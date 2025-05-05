85588c1ca4fd4a0e387b8350bd36b29500134935
Oleg Borzunov <olezhoncsgo@gmail.com>
2025-05-05 13:05:47 +0500

    checking issue in repository Test (clean Selenide)

---
+++ src/test/java/ru/vkusvillTests/selenideTests.java
@@ -0,0 +1,23 @@
+package ru.vkusvillTests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class selenideTests extends TestBase {
+
+@Test
+    public void myTest(){
+    SelenideLogger.addListener("allure", new AllureSelenide());
+    open("/Olezhonchik");
+    $x("//a[@href = '/Olezhonchik?tab=repositories']").click();
+    sleep(3000);
+    $x("//a[@href = '/Olezhonchik/xpath']").click();
+    $x("//a[@id = 'issues-tab']").click();
+    $x("//li[contains(@aria-label, '#12')]").should(Condition.exist);
+
+}
+}
